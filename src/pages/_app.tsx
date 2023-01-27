72a028bc7563f20211c3bb412e28a52fc41c30f9
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2023-01-27 11:28:47 +1100

    Change Styling
    - Change from Styled components to tailwind to faster development

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,26 @@
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "styled-components";
-import { Header } from "../components/Header";
-import { GlobalStyle } from "../styles/global";
-import { Container } from "../styles/pages/app";
-import { defaultTheme } from "../styles/themes/default";
+import Image from "next/image";
+import Link from "next/link";
 
+import '../styles/globals.css'
+import logo from "../assets/logo.svg"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <Container>
-          <Header />
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </Container>
-      </ThemeProvider>
+
+
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link href="/">
+          <Image src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+        <Link href="create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2  rounded-md" >
+          Create
+        </Link>
+      </header>
+
+      <Component {...pageProps} />
+
+
     </>
   );
 }
